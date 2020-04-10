76577517e7d1a8b44300516ff79f268f022f525f
Aleksandr Zh <zhuravlev.a.a@me.com>
2020-04-10 16:27:17 +0300

    подготавливаю пустой проект

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="table">
+    <h1 class="table__title">Table UI</h1>
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app',
+  data () {
+    return {
+
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+@import './style/style.scss';
+
+table {
+  width: 1140px;
+}
+
+</style>
