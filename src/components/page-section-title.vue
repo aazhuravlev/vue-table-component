b01951ed470cea47904cadd2a18f6a185d9aeeb6
Aleksandr Zh <zhuravlev.a.a@me.com>
2020-04-13 20:19:02 +0300

    создаю компонент для заголовка

---
+++ src/components/page-section-title.vue
@@ -0,0 +1,25 @@
+<template>
+    <div>
+        <slot></slot>
+        <hr>
+    </div>
+</template>
+
+<style lang="scss">
+@import '../style/style.scss';
+
+.page-section__title {
+    margin: 0;
+    margin-bottom: 16px;
+    font-size: 32px;
+    line-height: 40px;
+    font-weight: 600;
+}
+
+hr {
+    color: $separator-line-color;
+    opacity: 0.3;
+    margin: 0;
+    margin-bottom: 10px;
+}
+</style>
