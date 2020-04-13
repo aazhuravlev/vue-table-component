104f203bb925b309e7051605be701b8585f6ba26
Aleksandr Zh <zhuravlev.a.a@me.com>
2020-04-13 20:19:24 +0300

    создаю компонент для селекта и мультиселекта

---
+++ src/components/select.vue
@@ -0,0 +1,170 @@
+<template>
+    <span
+        class="select"
+        v-if="!isMultiselect"
+        @click="isItemsPerPageList = !isItemsPerPageList"
+    >
+            {{ itemsPerPage }} Per Page
+        <svg class="select__icon">
+            <use xlink:href="./src/images/sprites.svg#icon-arrow" />
+        </svg>
+
+        <ul
+            class="select__list"
+            v-if="isItemsPerPageList"
+        >
+            <li
+                class="select__item"
+                v-for="itemsCount in pageSize"
+                :key="itemsCount"
+                @click.stop="itemsPerPageClickHandler(itemsCount)"
+            >
+                {{ itemsCount }} Per Page
+            </li>
+        </ul>
+    </span>
+
+    <span
+        class="multiselect"
+        v-else
+        @click="isColumnList = !isColumnList"
+    >
+            {{ visibleTableHeaders.length }} columns selected
+        <svg class="multiselect__icon">
+            <use xlink:href="./src/images/sprites.svg#icon-arrow" />
+        </svg>
+
+        <ul
+            class="multiselect__list"
+            v-if="isColumnList"
+        >
+            <li class="multiselect__item">
+                <div class="checkbox" @click.stop>
+                    <input
+                        class="checkbox__input visually-hidden"
+                        ref="checkbox-column"
+                        type="checkbox"
+                        id="select-all-column"
+                        :checked="isAllColumnVisible"
+                        @change.stop="$emit('selectAllColumnHandler')"
+                    />
+                    <label
+                        class="checkbox__label"
+                        for="select-all-column"
+                    >
+                        <b>Select All</b>
+                    </label>
+                </div>
+            </li>
+            <li
+                class="multiselect__item"
+                v-for="(tableHeader, index) in tableHeaders"
+                :key="tableHeader.field"
+                @click.stop
+            >
+                <div class="checkbox">
+                    <input
+                        class="checkbox__input visually-hidden"
+                        type="checkbox"
+                        :id="tableHeader.field"
+                        :checked="tableHeader.visible"
+                        @change="$emit('changeHeaderVisibility', index)"
+                    />
+                    <label
+                        class="checkbox__label"
+                        :for="tableHeader.field"
+                    >
+                        {{ tableHeader.name }}
+                    </label>
+                </div>
+            </li>
+        </ul>
+    </span>
+</template>
+
+<script>
+export default {
+    props: [
+        'itemsPerPage',
+        'pageSize',
+        'isMultiselect',
+        'tableHeaders',
+        'isAllColumnVisible',
+        'visibleTableHeaders'
+    ],
+    data() {
+        return {
+            isItemsPerPageList: false,
+            isColumnList: false
+        }
+    },
+    methods: {
+        itemsPerPageClickHandler(itemsCount) {
+            this.isItemsPerPageList = false;
+            this.$emit('itemsPerPageClickHandler', itemsCount);
+        }
+    }
+}
+</script>
+
+<style lang="scss">
+@import '../style/style.scss';
+
+.select,
+.multiselect {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    position: relative;
+    min-width: 109px;
+    height: 32px;
+    padding: 3px 7px;
+    border: solid 1px $button-border-color;
+    border-radius: 2px;
+    box-sizing: border-box;
+    cursor: pointer;
+
+    &__icon {
+        width: 8px;
+        height: 8px;
+        margin-left: 5px;
+        fill: $arrow-icon-color;
+        transform: rotate(270deg);
+    }
+
+    &__list {
+        position: absolute;
+        z-index: 1;
+        top: 46px;
+        left: 0px;
+        width: 100%;
+        max-height: 243px;
+        margin: 0;
+        padding: 5px 0;
+        background-color: white;
+        list-style: none;
+        box-shadow: 0 2px 8px 0 $shadow-color;
+        overflow: auto;
+        border-radius: 2px;
+    }
+
+    &__item {
+        display: flex;
+        align-items: center;
+        padding: 5px 7px;
+    }
+}
+
+.multiselect {
+    min-width: 158px;
+
+    &__list {
+        width: 220px;
+        padding: 7px 0;
+    }
+
+    &__item {
+        padding: 0;
+    }
+}
+</style>
